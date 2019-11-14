9c1b895ec88b9c22627162b522e868c1e18e8b6c
William Newman <williamnewman@Williams-MBP.fios-router.home>
2019-11-13 22:07:05 -0500

    Added processing script and created M8 stop list

---
+++ process.py
@@ -0,0 +1,31 @@
+import csv
+
+def get_bus_stops(bus_line):
+    shape_ids = set()
+    with open('trips.txt') as trips:
+        trips_reader = csv.reader(trips, delimiter=',')
+        for row in trips_reader:
+            if row[0] == bus_line:
+                shape_ids.add(row[5])
+    
+    shape_ls = list(shape_ids)
+    stop_ls = []
+    with open('shapes.txt') as shapes:
+        shapes_reader = csv.reader(shapes, delimiter=',')
+        for row in shapes_reader:
+            if row[0] in shape_ids:
+                stop_ls.append(row)
+
+
+    stop_ls.sort()
+
+    return stop_ls
+
+def writeStops(stops, bus_line):
+    with open(bus_line + '.txt', 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(stops)
+
+
+stops = get_bus_stops("M8")
+writeStops(stops, "M8")
