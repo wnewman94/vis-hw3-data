c31be2385ea68b343a8f45e735f8222c15ceed6b
William Newman <williamnewman@Williams-MBP.fios-router.home>
2019-11-14 13:14:45 -0500

    Stopss

--- process.py
+++ process.py
@@ -1,32 +1,33 @@
 import csv
 
 def get_bus_stops(bus_line):
-    shape_ids = set()
-    with open('trips.txt') as trips:
+    stop_ids = set()
+    with open('stop_times.txt') as trips:
         trips_reader = csv.reader(trips, delimiter=',')
+        i = 0
         for row in trips_reader:
-            if row[0] == bus_line:
-                shape_ids.add(row[5])
-    
-    shape_ls = list(shape_ids)
+            if row[3] == 'stop_id':
+                continue
+            if i == 43:
+                break
+            stop_ids.add(row[3])
+            i += 1
+
     stop_ls = []
-    with open('shapes.txt') as shapes:
+    with open('stops.txt') as shapes:
         shapes_reader = csv.reader(shapes, delimiter=',')
         for row in shapes_reader:
-            if row[0] in shape_ids:
+            if row[0] in stop_ids:
                 stop_ls.append(row)
 
-    sorted(stop_ls, key=lambda stop: stop[3])
-    print stop_ls
 
     return stop_ls
 
 def writeStops(stops, bus_line):
-    with open(bus_line + '.txt', 'w') as writeFile:
+    with open(bus_line + '_stops.txt', 'w') as writeFile:
         writer = csv.writer(writeFile)
         writer.writerow(['id','lat','lng','seq'])
         writer.writerows(stops)
-
 
 stops = get_bus_stops("M8")
 writeStops(stops, "M8")
